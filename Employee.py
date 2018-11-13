98e037d8f8db72e381c12b73550597678796a031
yongze.chen <sapphire.php@gmail.com>
2018-11-13 14:40:06 +0800

    up

---
+++ Employee.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+class Employee:
+    '所有员工的基类'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print("TotalEmployee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name: ", self.name, ", Salary: ", self.salary)
